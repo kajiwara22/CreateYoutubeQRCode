708ed72dcf9fc88a6b6c298e64dff405fd06f741
KAJIWARAYutaka <20511091yk@grad.tama.ac.jp>
2015-08-10 00:07:39 +0900

    OAuth呼び出し用アプリ実装

---
+++ CreateYoutubeQRCodeConsoleWithOAuth/ConsoleOption.cs
@@ -0,0 +1,40 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateYoutubeQRCodeConsoleWithOAuth
+{
+    class ConsoleOption
+    {
+        [Option('o', "outputDir", Required = true, HelpText = "出力先フォルダパス")]
+        public string OutputDirPath
+        { get; set; }
+
+        [Option('c', "clientID", Required = true, HelpText = "OAuth Client ID")]
+        public String ClientID { get; set; }
+
+        [Option('s', "clientSeacret", Required = true, HelpText = "OAuth Client Seacret")]
+        public String ClientSeacret { get; set; }
+
+        //(3)HelpOption属性
+        [HelpOption(HelpText = "ヘルプを表示")]
+        public string GetUsage()
+        {
+            //ヘッダーの設定
+            HeadingInfo head = new HeadingInfo("CreateYoutubeQRCode", "Version 0.1");
+            HelpText help = new HelpText(head);
+            help.Copyright = new CopyrightInfo("KAJIWARA Yutaka", 2015);
+
+
+            //全オプションを表示(1行間隔)
+            help.AdditionalNewLineAfterOption = true;
+            help.AddOptions(this);
+
+            return help.ToString();
+        }
+    }
+}
---
+++ CreateYoutubeQRCodeConsoleWithOAuth/Program.cs
@@ -0,0 +1,39 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateYoutubeQRCodeConsoleWithOAuth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1)コマンドラインパーサーの初期化
+            ParserSettings setting = new ParserSettings();
+            setting.HelpWriter = Console.Error;
+            var parser = new Parser(setting);
+
+            //(2)オプション用クラスに引数を展開
+            var option = new ConsoleOption();
+            if (!parser.ParseArguments(args, option))
+            {
+                //パラメータに問題がある場合は、失敗ステータスで終了
+                Environment.Exit(1);
+            }
+            var filePath = System.IO.Path.GetFullPath(option.OutputDirPath);
+
+
+            var createYoutubeQRCode = new CreateYoutubeQRCodeLib.CreateYoutubeQRCode();
+            createYoutubeQRCode.TargetContentType = CreateYoutubeQRCodeLib.contentType.User;
+            createYoutubeQRCode.ConsumerKey = option.ClientID;
+            createYoutubeQRCode.ConsumerSecret = option.ClientSeacret;
+            createYoutubeQRCode.TargetFolderPath = filePath;
+
+            var t = createYoutubeQRCode.CreateQR();
+            t.Wait();
+        }
+    }
+}
